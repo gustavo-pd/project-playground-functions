d149d13c45d9465a6251e4e97b37187485e3abf7
GustavoPD <gudalmoro@gmail.com>
2021-06-29 19:08:15 -0300

    Primeiro commit, desafio 1 ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,12 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valor1, valor2) {
   // seu código aqui
+
+  if (valor1 === true && valor2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
